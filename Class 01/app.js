bd1f35acda2e2b4f36fc22b9b85912d37b8cb3ba
mustafashahzad845 <mustafashahzad845@gmail.com>
2025-12-11 14:46:50 +0500

    Javascript practice projects

---
+++ Class 01/app.js
@@ -0,0 +1,12 @@
+// alert("Hello World 1");
+// alert("Hello World 2");
+// alert("Hello World 3");
+
+var FirstName = "Mustafa"
+var LastName = "Shahzad"
+
+alert(FirstName + LastName);
+
+var gender = "Male"
+var gender = "Female"
+var cam
