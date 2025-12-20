c770a35050200f41c764e263e543104821428cee
mustafashahzad845 <mustafashahzad845@gmail.com>
2025-12-20 15:45:01 +0500

    Class 05 Practice

---
+++ Class 05 Practice/app.js
@@ -0,0 +1,133 @@
+// //  ==== Signup Data  ===
+// var signupEmail = prompt("Enter Your Signup Email")
+// var signupPassword = prompt("Enter Your Signup Password");
+// // ==== Login Data ====
+// var loginEmail = prompt("Enter your Login Email")
+// var loginPassword = prompt("Enter your Login Password")
+
+// if(signupEmail === loginEmail && signupPassword === loginPassword){
+//     alert("Login..")
+// }else{
+//     alert("invalid user")
+// }
+
+
+// Gender Wise campus
+// var gender = prompt("Enter your gender")
+
+// if(gender === "male") {
+//     console.log("Campus:","Head Office, Zaitoon Ashraf");
+// }else if(gender === "female"){
+//     console.log("Numaish,Aliabad");
+// }else if(gender === "Male"){
+//     console.log("Campus:","Head Office, Zaitoon Ashraf");
+// }else if (gender === "Female"){
+// console.log("Numaish,Aliabad");    
+// }else{
+//     console.log("campus not found");
+// }
+
+
+// === Marksheet ===
+// var english = +prompt("Enter your English marks")
+// var englishOral = +prompt("Enter Your English oral marks")
+// var Urdu = +prompt("Enter Your Urdu marks")
+// var urduOral = +prompt("Enter Your Urdu oral marks")
+// var maths = +prompt("Enter You Maths marks")
+// var mathsOral = +prompt("Enter Your Maths oral marks")
+// var Conversation = +prompt("Enter Your Conversation marks")
+// var Islamiat_Nazra = +prompt("Enter Your Islamiat and nazra marks")
+// var Computer = +prompt("Enter Your Computer marks")
+// var computerOral = +prompt("Enter Your computer oral marks")
+// var Drawing = +prompt("Enter Your Drawing marks")
+// var Science_SST = +prompt("Enter Your Science and S.St marks")
+
+// var obtainedMarks = english + englishOral + Urdu + urduOral + maths + mathsOral + Conversation + Islamiat_Nazra + Computer + computerOral + Drawing + Science_SST
+
+// var totalMarks = 600
+// var percentage = (obtainedMarks / totalMarks) * 100
+
+// console.log("Obtained Marks: " ,obtainedMarks);
+// console.log(percentage , "%");
+
+
+// if(percentage >= 80 && percentage <=100){
+//     console.log("Grade:", "A+");
+// }else if(percentage <80 && percentage>=70){
+//     console.log("Grade: ", "A");
+
+// }else{
+//     console.log("Grade: ", "F");
+
+// }
+
+
+
+
+
+// var num1 = +prompt("Enter your num1")
+// var num2 = +prompt("Enter your num2")
+// var opt = prompt("Enter your operator  Note: Operator must be  + , _ , * , /")
+
+// if (opt === "+") {
+//     console.log("Add: ", num1 + num2);
+
+// } else if (opt === "-") {
+//     console.log("Sub: ", num1 - num2);
+// } else if (opt === "*") {
+//     console.log("Mul: ", num1 * num2);
+// } else if (opt === "/") {
+//     console.log("Div: ", num1 / num2);
+// } else {
+//     console.log("operator not found");
+// }
+
+
+
+
+
+
+// var chemistry = prompt("Enter your Chemistry Marks")
+// var Physicss = prompt("Enter your Phsics Marks")
+// var english = prompt("Enter your English Marks")
+// var maths = prompt("Enter your Maths Marks")
+// var computer = prompt("Enter your Computer Marks")
+
+// document.writeln("<table border=1>")
+// document.writeln("<tr>")
+// document.writeln("<th>" + "Subject" + "<th>")
+// document.writeln("<th>" + "Obtained Marks" + "<th>")
+// document.writeln("<th>" + "Total Marks" + "<th>")
+// document.writeln("</tr>")
+
+
+
+// document.writeln("<tr>")
+// document.writeln("<td>" +  "Chemistry" + "</td>")
+// document.writeln("<td>" +  chemistry + "</td>")
+// document.writeln("<td>" +  75 + "</td>")
+// document.writeln("</tr>")
+
+// document.writeln("<tr>")
+// document.writeln("<td>" +  "Physicss" + "</td>")
+// document.writeln("<td>" +  75 + "</td>")
+// document.writeln("</tr>")
+
+// document.writeln("<tr>")
+// document.writeln("<td>" +  "english" + "</td>")
+// document.writeln("<td>" +  100 + "</td>")
+// document.writeln("</tr>")
+
+
+// document.writeln("<tr>")
+// document.writeln("<td>" +  "maths" + "</td>")
+// document.writeln("<td>" +  75 + "</td>")
+// document.writeln("</tr>")
+
+
+// document.writeln("<tr>")
+// document.writeln("<td>" +  "computer" + "</td>")
+// document.writeln("<td>" +  75 + "</td>")
+// document.writeln("</tr>")
+
+// document.writeln("</table>")
