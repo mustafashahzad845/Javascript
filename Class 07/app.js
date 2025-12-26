18654203364b707fe86dd7f3ede38edc8fc8b4d6
mustafashahzad845 <mustafashahzad845@gmail.com>
2025-12-27 01:14:44 +0500

    Array Methods

---
+++ Class 07/app.js
@@ -0,0 +1,43 @@
+// var cities = []
+// console.log(cities);
+
+// cities[0] = "Karachi"
+// cities[1] = "Lahore"
+// cities[2] = "Multan"
+// cities[3] = "Rawalpindi"
+// cities[5] = "Peshawar"
+
+
+// cities[4] = "Swat"
+// // console.log(cities);
+
+// // cities.pop()
+// // cities.pop()
+// // cities.pop()
+// // cities.pop()
+// // cities.pop()
+
+// cities.push("Pakistan")
+// console.log(cities , "Original Array");
+// // cities.shift()
+// // cities.shift()
+// // cities.shift()
+// // cities.shift()
+// // cities.unshift("Punjab")
+// // cities.unshift("Sindh" , "Punjab")
+
+// // === Slice ====
+// //['Karachi', 'Lahore', 'Multan', 'Rawalpindi', 'Swat', 'Peshawar', 'Pakistan']
+// var newArray = cities.slice(-4 , -3)
+
+// console.log(newArray , "new array");
+
+
+
+// var userName = ["Mustafa" , "Shahzad" , "Ahmed" , "Mahad" , "Shafay" , "Ali" , "Raza"]
+// console.log(userName , "Original Array");
+// var newArray = userName.slice(-6 , 2 )
+// console.log(newArray , "NewArray");
+
+
+
