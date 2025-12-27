94ac3f71d5efb78de0c46939446cf3776c715ad7
mustafashahzad845 <mustafashahzad845@gmail.com>
2025-12-27 13:58:32 +0500

    Array methods Practice

---
+++ Class 07 Practice/app.js
@@ -0,0 +1,30 @@
+var foods = ["biryani" , "pulao" , "Qorma" , "Karahi" , "Tikka"]
+console.log(foods);
+
+// //  === Pop ===
+// // foods.pop()
+// // foods.pop()
+// // foods.pop()
+// // foods.pop()
+// // foods.pop()
+
+// // === Push ===
+// // foods.push("Kheer")
+// // foods.push("Chumchum")
+
+// // === Shift ===
+// // foods.shift()
+// // foods.shift()
+// // foods.shift()
+// // foods.shift()
+// // foods.shift()
+
+// // === Unshift ===
+// // foods.unshift("Kabab" , "Paratha")
+
+
+var newArray = foods.slice(-3 , 1)
+
+console.log(newArray , "new array");
+
+
