226c0db6ec967c1ddb679a61fd6dab8f88502cd5
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-04 16:35:26 +0500

    For-Loop Assignmnet

---
+++ For loop Assignment/app.js
@@ -0,0 +1,31 @@
+// var numbers  = [ 1 , 2 ,3 ,4 , 5 ,6, 7, 8 ,9 ,10]
+// var numberMatch = "Not Match"
+// var numberCheck = +prompt("Enter number")
+
+// for(var i = 0 ; i < numbers.length ; i++){
+
+//     if(numberCheck === numbers[i]) {
+//         alert("Number Found")
+//         numberMatch = "Match Successfully"
+//         document.writeln(numberMatch)
+
+//         break
+//     }
+// }
+
+// if (numberMatch === "Not Match") {
+//     alert("Number Not Found")
+//     document.writeln(numberMatch)
+// }
+
+// var numbers = []
+
+// for (var i = 0 ; i < 100 ; i++) {
+
+//     numbers[i] = i + 1
+
+//     document.writeln(numbers[i] , "<br />")
+// }
+
+
+// console.log(numbers[0]);
