b517dc501c6c765695cbcc239f6d1ab87d731c19
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-06 00:21:54 +0500

    Flag and not-equal-to

---
+++ Class 09 Practice 2.0/app.js
@@ -0,0 +1,111 @@
+// var numbers = [1 , 2 , 3 , 4 , 5, 6 , 7 , 8 , 9 ,10]
+// var checkNumber = +prompt("Enter number")
+// var number_search = "Number nahi mila"
+
+// for (var i = 0 ; i < numbers.length ; i++)  {
+
+//     if (checkNumber === numbers[i]) {
+        
+//         alert("Nuumber Found😊")
+//         number_search = "Number milgaya"
+//         console.log(number_search);
+                
+//         break
+//     }
+// }
+
+
+// if(number_search === "Number nahi mila"){
+//     alert("Number not Found😒")
+//     console.log(number_search);
+    
+// }
+
+
+// for (var i = 100 ; i >= -100 ; i--){
+
+//     console.log(i);
+    
+// }
+
+// var startingNumber = +prompt("Enter Starting Number")
+// var endingNumber = +prompt("Enter ending Number")
+
+// for (var i = startingNumber ; i >= endingNumber ; i-- ){
+
+//     document.writeln(i ,"<br />")
+// }
+
+
+
+// var startingNumber = +prompt("Enter Starting Number")
+// var endingNumber = +prompt("Enter ending Number")
+// var even_odd_Normal = prompt("Which type of Number you want?   Even , Odd, Normal").trim()
+// var backward_forward = prompt("Which type of counting you want?     Backward or Forward").trim()
+
+
+// if(backward_forward === "Forward" || backward_forward ==="forward"){
+
+// for(var i = startingNumber ; i <= endingNumber ; i++){
+
+//    if(even_odd_Normal === "Even" || even_odd_Normal === "even"){
+
+//     if(i % 2 === 0){
+//         document.writeln("Forward Counting" , "(Even Number)" , i , "<br />")
+//     }
+//    }else if(even_odd_Normal === "Odd" || even_odd_Normal === "odd"){
+
+//     if(i % 2 !== 0){
+//         document.writeln("Forward Counting" , "(Odd Number)" , i , "<br />")
+//     }
+//    }else if(even_odd_Normal === "Normal" || even_odd_Normal === "normal"){
+//     document.writeln("Forward Counting" , i , "<br />")
+//    }
+
+// }
+
+//     }else if(backward_forward === "Backward" || backward_forward === "backward"){
+
+//         for (var i = startingNumber ; i >= endingNumber ; i--){
+
+//             if(even_odd_Normal === "Even" || even_odd_Normal === "even"){
+
+//                 if(i%2 === 0){
+//                             document.writeln("Backwrad Counting" , "(Even Number)" , i , "<br />")
+
+//                 }
+//             }else if(even_odd_Normal === "Odd" || even_odd_Normal === "odd"){
+//                 if(i % 2 !== 0){
+//                             document.writeln("Backward Counting" , "(Odd Number)" , i , "<br />")
+
+//                 }
+//             }else if(even_odd_Normal === "Noraml" || even_odd_Normal === "normal"){
+//                     document.writeln("Backjward Counting" , i , "<br />")
+
+//             }
+//         }
+//     }
+
+
+// var numbers = [];
+
+
+
+// for(var i = 0 ; i <= 99 ; i++){
+
+//     numbers[i] = i+1
+// }
+
+
+// console.log(numbers);
+
+
+// for (var i = 0 ; i <= 10 ; i++){
+    
+//     if(i === 0){
+//         continue
+//     }
+
+//     console.log(i);
+    
+// }
---
+++ Class 10/app.js
@@ -0,0 +1,74 @@
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// var numberFound = false
+
+// for(var i = 0 ; i < numbers.length ; i++){
+
+//     if(numbers[i] === 5){
+//         alert("Value Milgayi")
+//         numberFound = true
+//         console.log(numberFound);
+                
+//         break
+    
+//     }
+// }
+
+// if(numberFound === false){
+// alert("Value nahi mili")
+// console.log(numberFound);
+
+// }
+
+
+// var isAssignment = prompt("Can you complete your assignment?  Yes or No").toUpperCase()
+
+// console.log(isAssignment);
+
+// if(isAssignment === "Yes"|| isAssignment === "yes" || isAssignment === "YES" || isAssignment === "No" || isAssignment === "NO" || isAssignment === "no"){
+
+// if(isAssignment === "Yes" || isAssignment === "yes" || isAssignment === "YES"){
+//     alert("Aajao bhai")
+// }else if(isAssignment === "No" || isAssignment === "NO" || isAssignment === "no"){
+//     alert("Bye Bye")
+// }
+
+// }else{
+//     alert("Enter only yes or no")
+// }
+
+
+// var numbers = []
+
+// for (var i = 0 ; i < 100 ; i++){
+    
+//     numbers[i] = i+1
+// }
+
+// console.log(numbers);
+
+
+
+// numbers.pop()
+// console.log(numbers);
+// for (var i = 1 ; i <= 100 ; i++){
+//     numbers.push(i)
+// }
+
+// console.log(numbers);
+
+
+// var newArr = numbers.toSpliced(3, 3)
+
+// console.log(numbers);
+
+// console.log(newArr);
+
+
+// var email = prompt("Enter email address")
+// var password = prompt("Enter password")
+// var isCheck = true
+
+// if(!email || !password){
+//     console.log(isCheck);
+    
+// }
