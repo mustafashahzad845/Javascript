938d6e7684e5f4f8a8f8370e038f1b3c8324bafd
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-15 17:39:14 +0500

    Clas 12 Practice

---
+++ Class 12 Practice/app.js
@@ -0,0 +1,97 @@
+// var text = 'My name is "Mustafa" "shidfy ' 
+// console.log(text);
+
+
+// var text = "Physics is the fundamental science studying matter, energy, force, and motion, exploring how the universe works from the smallest particles to the cosmos, aiming to discover universal laws governing phenomena like gravity, light, and electricity through observation, experimentation, and mathematical principles."
+
+
+// for(var i = 0 ; i < text.length ; i++) {
+
+//     // console.log(text.slice(i , i + 4));
+    
+//     if(text.slice(i , i + 4) === "laws"){
+//         console.log(i);
+
+//         var part1 = text.slice(0 , i)
+//         console.log(part1);
+        
+//         var part2 = text.slice(i+4)
+//         console.log(part2);
+
+//         text = part1 + "rules"  + part2
+        
+//     }
+// }
+
+
+// console.log(text);
+
+// console.log(text[184]);
+// console.log(text[185]);
+// console.log(text[186]);
+// console.log(text[187]);
+
+// html
+// h = 0  , 1
+// t = 1  , 2 
+// m = 2  , 3
+// l = 3  , 4
+
+
+
+
+
+// var para = "Bootstrap is a free front-end framework for faster and easier web development.Bootstrap includes HTML and CSS based design templates"
+
+// // console.log(para);
+
+
+// for (var i = 0 ; i < para.length ; i++){
+
+//     // console.log(para.slice(i , i + 6));
+    
+//     if(para.slice(i , i + 6) === "faster") {
+//         // console.log("index number" , i);
+
+//          var part1 = para.slice(0 , i)
+//         //  console.log(part1);
+         
+//         var part2 = para.slice(i+6)
+//         // console.log(part2);
+     
+//         para = part1 + "quicker" + part2
+//         console.log(para);
+        
+//     }
+// }
+
+
+
+var text = "Biology is the scientific study of life, exploring living organisms, their vital processes, structures, functions, origins, evolution, and interactions with each other and their environment, encompassing everything from the molecular level within cells to entire ecosystems and diverse fields like botany, zoology, genetics, and ecology. It's a broad natural science that uses unifying principles, like the cell as the basic unit, genes, evolution, energy, and homeostasis, to understand the diversity of living things."
+
+// console.log(text);
+
+// find index number
+
+for (var i = 0 ; i<text.length ; i++) {
+// text.slice(i)
+
+// console.log(text.slice(i , i+6));
+
+if (text.slice(i , i+6) === "energy") {
+console.log("indexnumber" , i);
+
+var part1  = text.slice(0 , i)
+console.log(part1);
+
+var part2 = text.slice(i+6)
+console.log(part2);
+
+
+text = part1 + "power" +part2
+
+console.log(text);
+
+// break
+}
+}
