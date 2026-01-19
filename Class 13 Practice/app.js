1c72817a73cc4d51fc62480d59b64f0eb8a04e61
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-19 20:03:39 +0500

    Class 13 Practice

---
+++ Class 13 Practice/app.js
@@ -0,0 +1,19 @@
+// var randomNumber = Math.floor(Math.random() * (20 - 5 + 1) + 5)
+// console.log(randomNumber);
+
+
+// var text = "My name is MUstafa."
+// var textArr = text.split(" ")
+// // console.log(textArr);
+// var finalWord = []
+
+// for(var i = 0 ; i < textArr.length ; i++){
+
+//     var word = textArr[i].slice(0 , 1).toLocaleUpperCase() + textArr[i].slice(1).toLocaleLowerCase()
+//     // console.log(word);
+    
+//      finalWord.push(word)
+// }
+
+
+// console.log(word);
