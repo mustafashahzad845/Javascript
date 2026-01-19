c2b17dedbc200416ae2aa69beec9ad2370ff0643
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-20 01:36:47 +0500

    Current Date Time , Function's

---
+++ Class 14/app.js
@@ -0,0 +1,96 @@
+// var now = new Date()
+// console.log(now , typeof now);
+
+// var now = new Date().toLocaleTimeString()
+// now = new Date().toTimeString()
+// console.log(now);
+
+// var now = new Date().getTime()/ (1000 *60* 60 * 24 )
+// console.log(Math.round(now));
+
+// var now = new Date().toString()
+// var currentDay = now.slice(0 ,3)
+// console.log(now);
+// console.log(currentDay);
+
+// var day = new Date("20 Jan 2024").getTime()
+// console.log(day);
+
+// var now = new Date().getDay()
+// console.log(now);
+
+
+
+// DOB Calculator
+// var userBirthday = prompt("Enter your DOB" , "20 Jan 2010")
+// var currentDate = new Date()
+// console.log(userBirthday);
+// console.log(currentDate);
+
+
+// var userBirthdayMilliSec = new Date(userBirthday).getTime()
+// var currentDateMilliSec = currentDate.getTime()
+
+// console.log(userBirthdayMilliSec);
+// console.log(currentDateMilliSec);
+
+
+// var diff = currentDateMilliSec - userBirthdayMilliSec
+// // console.log("diff" , diff);
+// var userAge = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+//   console.log("Mubark bhai ap " , userAge, "years" , " saal ke hogae 😊");
+  
+
+
+
+//   var date = new Date("Jan 21,2000")
+//   console.log("date" , date);
+  
+
+
+// function myFunction() {
+//     console.log("Hello WOrld 1");
+//     console.log("Hello WOrld 2");
+//     console.log("Hello WOrld 3");
+    
+// }
+
+// myFunction()
+
+
+// function alert_function(hello) {
+// alert("Mustafa" + hello)
+
+// }
+
+// alert_function("Sain")
+
+
+// function calc_function(num1 , num2){
+
+//     // var num1 = +prompt("Enter num1")
+//     // var opt = prompt("Enter your opt")
+//     // var num2 = +prompt("Enter num2")
+
+//     // if(opt == "+"){
+//     //     alert(num1 + num2);
+        
+//     // }else if(opt == "-"){
+//     //     alert(num1 - num2)
+//     // }else if(opt == "*"){
+//     //     alert(num1 * num2)
+//     // }else if(opt == "/"){
+//     //     alert(num1 / num2)
+
+//     // }
+
+// num1 = prompt("Enter num1")
+//     // var num1 = 300
+//     // var num2 = 500
+//      var opt = prompt("Enter opt")
+
+//      console.log(num1 , opt , num2);
+     
+// }
+
+// calc_function(500 , 300)
