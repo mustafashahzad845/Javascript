bc555f6f2da6b2b4ffd82c479cd008c65c154393
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-22 23:50:09 +0500

    Class 14 Practice

---
+++ Class 14 Practice/app.js
@@ -0,0 +1,72 @@
+// var now = new Date()
+// console.log(now);
+
+
+// var now = new Date()
+// console.log( "Original" , now);
+
+// var now = new Date().toDateString()
+// console.log(now);
+
+// var now = new Date().toLocaleDateString()
+// console.log(now);
+
+// var now = new Date().toTimeString()
+// console.log(now);
+
+
+// var now = new Date().toLocaleTimeString()
+// console.log(now);
+
+
+// var now = new Date().getTime() / (1000 * 60 * 60 * 24 * 365)
+// console.log(Math.floor(now));
+
+
+// var userBirthday = new Date("25 Feb 2010").getTime()
+// console.log(userBirthday);
+
+// var currentTime = new Date().getTime()
+
+// var  userAge  = currentTime - userBirthday
+// console.log(Math.floor(userAge / (1000 * 60 * 60 * 24 * 365)));
+
+
+// var userBirthday = prompt("Enter your DOB")
+// // console.log(userBirthday);
+// userBirthday = new Date(userBirthday).getTime()
+// console.log(userBirthday);
+
+// var currentTime = new Date().getTime()
+// var userAge = currentTime - userBirthday
+// userAge = Math.floor(userAge / (1000 * 60 * 60 * 24 * 365))
+// alert("Mubarak bhai aap " + userAge + " saal ke hogae 😊")
+
+
+// var currentDate = new Date().getTime()
+// console.log(currentDate);
+
+// var userDOB = prompt("Enter your DOB" , "1 Jan 2026")
+// userDOB = new Date(userDOB).getTime()
+// console.log(userDOB);
+
+// var userAge = currentDate - userDOB
+// userAge = Math.floor(userAge / (1000 * 60 * 60 * 24 * 365))
+// alert("Mubarak bhai app " + userAge + " saal ke hogae😊")
+
+
+// var  now = new Date("20 Jan 2010").getSeconds()
+// console.log(now);
+
+
+var str = "sAylani"
+// console.log(str.toLocaleLowerCase());
+
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.slice(0,1).toLocaleUpperCase() + str.slice(1).toLowerCase());
+
+
+
+
+
