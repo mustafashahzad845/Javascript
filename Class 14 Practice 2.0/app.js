27fb943e67f5b9e57e82419d9fddae593b940e3e
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-23 18:00:12 +0500

    Multiple Capitialize

---
+++ Class 14 Practice 2.0/app.js
@@ -0,0 +1,109 @@
+// var str = "my name is Mustafa"
+// str = str.split(" ").reverse() ;
+// str = str.join(" ")
+// console.log(str);
+
+
+
+
+// var para = "my name is mustafa"
+// console.log("original para: " , para);
+
+// var paraArr = para.split(" ")
+// // console.log("paraArr" , paraArr);
+// var arr = []
+
+
+// for(var i = 0 ; i < paraArr.length ; i++) {
+
+//     var word = paraArr[i]
+//     // console.log(word);
+    
+//     //Capitialize
+//     word = word.slice(0 , 1).toUpperCase() + word.slice(1).toLocaleLowerCase()
+//     // console.log(word);
+    
+//     arr.push(word)
+// }
+
+
+// // console.log(arr);
+// var Capitializepara = arr.join(" ")
+// console.log("Capitializepara: " , Capitializepara);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// H - 0 - 1
+// T - 1 - 2
+// M  - 2 - 3
+//L  - 3 - 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var para = prompt("Enter some text" , "my name is mustafa")
+// var paraArr = para.split(" ")
+// console.log(paraArr);
+// var arr = []
+
+
+// for(var i = 0 ; i < paraArr.length ; i++) {
+
+//     var word = paraArr[i]
+//     // console.log(word);
+    
+//     word = word.slice(0 , 1).toUpperCase() + word.slice(1).toLowerCase()
+    
+//     arr.push(word)
+// }
+
+//     console.log(arr);
+//     var Capitializepara = arr.join(" ")
+//     console.log(Capitializepara);
+    
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
