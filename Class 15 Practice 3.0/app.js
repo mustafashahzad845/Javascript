00ac423bd2728c8376bd205fa0bd9dff1f6dcdf3
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-01-26 15:16:33 +0500

    Function(Parameters and Arguements , Return keyword)

---
+++ Class 15 Practice 3.0/app.js
@@ -0,0 +1,30 @@
+
+
+// function log_function() {
+
+// console.log("Hello World 1");
+// console.log("Hello World 2");
+
+// }
+// function greetFunction(name , secondName , thirdName) {
+
+    // alert("Hello " + name)
+    // alert("Hello " + secondName)
+    // alert("Hello " + thirdName)
+// }
+
+// greetFunction("Mustafa" , "Ali" , "Shahzad")
+// greetFunction("Shahzad")
+// greetFunction("Ali")
+
+
+function calc(num1= 0 , num2 = 0){
+
+    var res = num1 + num2
+    console.log(res);
+ return res   
+}
+
+
+
+// calc(300)
