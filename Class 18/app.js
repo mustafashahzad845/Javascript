e00a14b4c4e8ddeea74c00a09d8ef8703a187f98
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-02-03 01:01:50 +0500

    Events: onfocus , onblur , styling through js , get Element by tag name

---
+++ Class 18/app.js
@@ -0,0 +1,80 @@
+// function focusFunction(element , number) {
+// console.log("focusFunction" , element , number);
+
+// }
+
+// function blurFunction(element , number) {
+//     console.log("blurFunction" , element , number);
+
+// }
+
+// function passwordManage(element , flag) {
+
+
+// console.log(element , flag);
+
+
+// if(flag == "focus"){
+//     element.type = "text"
+// }else{
+//     element.type ="password"
+// }
+    
+// }
+
+
+
+
+// function userName(element , flag) {
+// console.log(element , flag);
+
+
+// console.log();
+
+
+
+// if(flag == "focus"){
+//     element.style.outline = "none"
+
+// element.style.backgroundColor = "blue"
+// }else{
+//     element.style.border = ""
+
+//     if(element.value.length >= 3){
+// element.style.backgroundColor = "green"
+//     // console.log(element.value.length);
+
+//     }else{
+// element.style.backgroundColor = "red"
+//     // console.log(element.value.lenght);
+//     }
+
+// }
+
+
+// }
+
+
+
+
+// function foo() {
+//     console.log("Hello World!");
+    
+// }
+
+
+// function Paras() {
+//     var paras = document.getElementsByTagName("p")
+//     // console.log(paras);
+
+//     // console.log(paras[2]);
+
+//     paras[2].style.color = "red" ;
+ 
+    
+//     var parent = document.getElementById("parent")
+
+//     var child = parent.getElementsByTagName("p")
+//     console.log(child);
+    
+// }
