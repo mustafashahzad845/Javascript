3b7bd24d9fc6bc918806435f96685a1842a7d788
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-02-07 16:01:06 +0500

    dom completed

---
+++ Class 19/app.js
@@ -0,0 +1,87 @@
+/// <reference lib="dom" />
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[2]);
+// var element =  document.childNodes[1].childNodes[2].childNodes[1].style.color = "red"
+
+// console.log(document.children[0].children[1].children[0]);
+// var elem =  document.children[0].children[1].children[0]
+// elem.style.color = "green"
+
+// var paras = document.getElementsByTagName("p")
+// console.log(paras);
+
+// console.log(document.childNodes[1].childNodes[0].childNodes[7]);
+
+// var cssLink = document.childNodes[1].childNodes[0].childNodes[7].href = "mystyle.css"
+
+// console.log(document.children[0].children[1].children);
+
+// var element = document.children[0].children[1].children[0].style.color = "red" 
+
+// var parent = document.getElementById("parent")
+// console.log(parent.childNodes);
+// console.log(parent.children);
+
+// var firstElem = parent.firstElementChild
+// firstElem.style.color = "blue"
+
+// var lastElem = parent.lastE
+// lastElem.style.color = "green"
+// console.log(lastElem);
+
+
+
+// var middleChild = parent.lastElementChild
+
+    // console.log(middleChild, "middleChild");
+
+// console.log(parent.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.previousElementSibling.previousElementSibling.previousElementSibling);
+
+
+// console.log(parent.lastElementChild);
+
+// function attributeValue() {
+// var input = document.getElementById("input")
+//     console.log(input);
+
+//     if(input.hasAttribute("placeholder")){
+//         console.log(   "exist"   ,   input.hasAttribute("placeholder"));
+        
+        
+//     }else{
+//                 console.log("not exist" , input.hasAttribute("placeholder"));
+
+//     var setAttribute = input.setAttribute("placeholder", "Enter your name")
+//     var getAttribute = input.getAttribute("placeholder")
+// console.log(setAttribute , "setAttribute");
+// console.log(getAttribute , "getAttribute");
+
+//     }
+    
+    // console.log();
+    // var getAttribute = input.getAttribute("placeholder")
+// }
+
+
+    
+// var conatiner = document.getElementById("conatiner")
+// var element = document.createElement("h1")
+// console.log(element);
+
+// element.innerHTML = "Hello World!"
+// // element.style.color = "red"
+// element.className = "h1"
+// conatiner.append(element)
+
+//     var conatiner = document.getElementById("conatiner")
+// var inputElement = document.createElement("input")
+// console.log(inputElement);
+// // inputElement.type = "text"
+// // inputElement.type = "text"
+// inputElement.setAttribute("type", "text")
+// inputElement.setAttribute("placeholder", "Enter your Name")
+// var inputUI = conatiner.appendChild(inputElement)
+
+// console.log(inputUI);
+
+
