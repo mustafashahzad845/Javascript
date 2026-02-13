e32bc0bb3b72e1693c493dd81e3840545cbad1de
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-02-13 19:41:56 +0500

    Sticky Notes

---
+++ Class  20 Practice 2.0/app.js
@@ -0,0 +1,25 @@
+// var firstName = `Mustafa`
+// var lastName = `Shahzad`
+
+// console.log(`Username: ${firstName} ${lastName}`);
+
+
+// var parent = document.getElementById("parent")
+// var h1Element = document.createElement("h1")
+// console.log(h1Element);
+
+// var childText = document.createTextNode(`Hello World`)
+//  h1Element.append(childText)
+
+// parent.append(h1Element)  
+
+
+
+var parent = document.getElementById("parent")
+var userInput = document.getElementById("userInput")
+
+function addData(){
+console.log(userInput.value);
+
+}
+
