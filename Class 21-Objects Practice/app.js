821270984bdcc532827a008a19e245b8f8a56ebb
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-02-14 21:21:51 +0500

    Objects Practice

---
+++ Class 21-Objects Practice/app.js
@@ -0,0 +1,56 @@
+// var std = {
+//     name : `Mustafa`,
+//     fatherName : `Shahzad`,
+//     city : `Karachi`,
+//     age : 15 , 
+//     status : `Active` , 
+//     rollNo : 450689,
+
+// }
+
+
+// std.name = `Usman`
+// std.fatherName = `Rizwan`
+// std.age= `20 years`
+// std.rollNo= `98212`
+// std.email = `usman@gmail.com`
+
+// std.rollNo = undefined
+
+// std.key = `keyValue`
+// // delete std.rollNo
+
+
+// // console.log("address" in std)
+
+
+// // std.age = 19
+
+// // console.log(std.age);
+
+// // delete std.age
+
+// console.log(std);
+
+
+// for(var key in std){
+//     console.log(`${key} : ${std.key}`);
+    
+// }
+
+
+
+//  function checkName() {
+//     var users = [`Mustafa` , `Shahzad` , `Usman` , `Ali` , `Raza`]
+// var userName = prompt(`Enter your name`)
+
+
+
+//     if(users.indexOf(userName) === -1){
+//         alert(`Bhai apka naam nhi hai`)
+//     }else{
+// alert(`Apka naam hai😊`)
+//     }
+// }
+
+
