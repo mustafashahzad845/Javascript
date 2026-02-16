64ad8548d4dc44767d33cd70957be8929f64ba3b
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-02-17 02:12:55 +0500

    Objects Advance

---
+++ Class 22-Objects Advance/app.js
@@ -0,0 +1,115 @@
+// var myTable = document.getElementById("myTable")
+// var totalScore = 0;
+// var std = {
+//     name : "Mustafa",
+//     fatherName : "Shahzad" , 
+//  subjects : ["html" , "css" , "js" , "react" , "nodejs"],
+//  scores : {
+// html : 70,
+// css :  85,  
+// js : 65,
+// react : 40,
+// nodejs : 95
+//  },
+
+//  finalScore : function() {
+
+//  }
+// }
+
+
+// var loopLength = std.subjects.length
+
+// for(var i = 0 ; i < loopLength ; i++ ){
+//     // console.log(std.subjects[i]);
+
+//     var subjectsName = std.subjects[i];
+//     var subjectScores = std.scores[subjectsName]
+//     // console.log(subjectScores);
+//     totalScore += std.scores[subjectsName]
+// // console.log(totalScore);
+
+
+// var tableRow = `<tr> 
+// <td> ${subjectsName} </td>
+// <td> ${subjectScores} </td>
+// </tr>`
+
+// myTable.innerHTML += tableRow
+
+
+
+
+// }
+
+
+// var tableLastRow = `<tr>
+// <td>Total</td>
+// <td> ${totalScore} </td>
+// </tr>`
+
+// myTable.innerHTML += tableLastRow
+
+// console.log(totalScore);
+
+// console.log(std);
+
+
+
+// var user = {
+//     name : "Mustafa",
+//     lastName : "Shahzad",
+//     fullName : function() {
+//         console.log(`${user.name} ${user.lastName}`);
+//         // return fullName
+
+//         var fullName = `${user.name} ${user.lastName}`
+//         return fullName
+//     }
+// }
+
+// var finalName =  user.fullName()
+// console.log(finalName);
+
+
+
+
+
+
+
+var parent = document.getElementById("parent")
+
+
+
+
+
+var users = [
+    {
+name : "Mustafa",
+email : "mustafa@gmail.com",
+address : {
+    city : "Karachi",
+    country : "Pakistan"
+},
+skill : ["html" , "css" , "bootstrap","github" , "js"]
+
+},
+
+{
+name : "Shahzad",
+email : "shahzad@gmail.com",
+address : {
+    city : "Karachi",
+    country : "Pakistan"
+},
+skill : ["html" , "css" , "nodejs","github" , "js"]
+}
+]
+
+
+
+
+// console.log(user2);
+
+
+// console.log(user.skill[2]);
