3ca52e4ac520895fabc2932bc8ebbab0a8849f68
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-05 18:17:59 +0500

    Stop Watch Application

---
+++ Class 26-Stop Watch/app.js
@@ -0,0 +1,75 @@
+var timeCounter = document.getElementById("timeCounter")
+
+
+var hour = 0;
+var minute = 0;
+var sec = 0;
+var millisec = 0;
+
+
+        timeCounter.innerHTML = `0${hour} : 0${minute} : 0${sec} : 0${millisec}`
+
+var timer ;
+
+  function startWatch() {
+    console.log("startWatch");
+    
+  timer = setInterval(
+        function() {
+millisec++
+
+
+        timeCounter.innerHTML = `${ hour < 10 ? '0'+hour : hour} : ${ minute < 10 ? '0'+minute : minute} : ${ sec < 10 ? '0'+sec : sec} : ${millisec < 10 ? '0' + millisec : millisec}`
+
+        if(millisec == 99){
+            `${millisec = 0}0`
+            sec++
+        }
+
+if(sec >= 10){
+  
+}
+
+        if(sec == 60){
+            minute++
+           `${sec = 0}0`
+        }
+
+        }
+        ,
+      
+         10)
+
+  
+return timeCounter
+    }
+
+
+
+
+ 
+    
+
+
+    function pauseWatch() {
+console.log(timeCounter);
+
+
+
+clearInterval(timer)
+
+return
+    }
+        
+
+    function resetWatch() {
+        console.log("resetWatch");
+
+hour = 0 ;
+minute = 0 ;
+sec = 0 ;
+millisec = 0 
+
+        timeCounter.innerHTML = timeCounter.innerHTML = `0${hour} : 0${minute} : 0${sec} : 0${millisec}`
+        
+    }
