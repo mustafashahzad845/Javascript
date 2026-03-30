652d338279b0d808b1980245bbf216a80a4013e8
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-30 19:18:45 +0500

    Tic Tac Toe

--- Tic Tac Toe Application/app.js
+++ Tic Tac Toe Application/app.js
@@ -1,10 +1,17 @@
 var userTurn = 0;
+var alltdElem = document.getElementsByTagName("td")
 function clickFun(td) {
     userTurn++
 
 if(userTurn % 2 !== 0){
     td.innerHTML = "X"
     td.style.color = "red"
+
+    if(alltdElem[0].innerHTML === "X" && alltdElem[1].innerHTML === "X" && alltdElem[2].innerHTML === "X" ){
+// alert("You Win")
+return
+    }
+
 }else{
     td.innerHTML = "0"
     td.style.color = "blue"
