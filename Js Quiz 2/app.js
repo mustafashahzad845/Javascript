03edcd1c5bca9520eaa49610a2560f1ca4f9b23e
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-21 18:40:55 +0500

    JS Quiz 2 Preparation Code

---
+++ Js Quiz 2/app.js
@@ -0,0 +1,55 @@
+// const str = "Hello"
+// console.log(str.indexOf("l"));
+// const numbers ="10" % "10"
+// console.log(numbers , typeof numbers);
+
+// function sum (a , b){
+    // return a+b
+// }
+
+// alert(sum(2, 3))
+
+// const text = "Hello"
+// alert(text.toLocaleLowerCase().charAt(1))
+
+// for(var i=1; i<=3; i++){
+//   for(var j=1; j<=2; j++){
+//     console.log("*");
+    
+//   }
+// }
+
+
+// function test() {
+//     var x = 5
+// }
+
+// alert(x)
+
+
+
+
+
+// Q19
+// var num = 5.6789;
+// alert(num.toFixed(2));
+
+// Output?
+
+// A) 5.6
+// B) 5.67
+// C) 5.68
+// D) 6
+
+
+
+// let num = 32
+// num = JSON.stringify(num)
+// num = JSON.parse(num)
+// console.log(num);
+
+
+
+let num = 78.54754
+// num = parseFloat(num.toFixed(2))
+console.log(num );
