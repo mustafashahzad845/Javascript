da998dbb82c0d9b6edbe8cac23c74f11aae3d04d
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-29 02:00:24 +0500

    JS Book Whole Practice

---
+++ Whole js Book Practie/app.js
@@ -0,0 +1,67 @@
+// const user = `Mustafa`;
+// const greet = "Hello"
+// alert(`${greet} ${user}`);
+
+// const  a = alert("Hello World")
+// console.log(a , typeof a);
+
+// alert(`Hello`)
+
+
+
+// var a = 15;
+// console.log(a);
+
+
+// const b = 6
+// console.log(b);
+ 
+// var a= 87
+// var a = 84
+// console.log(a);
+
+// console.log(alert("Hello World"));
+
+// const a = "Mustafa"
+// console.log(a);
+
+
+// const user = prompt("Enter your name" , "Ali" , "Mustafa")              
+// console.log(user);
+
+// function num() {
+// if(1 == 1) {
+    // alert("Hello")  
+    // alert("Hello 2")
+
+// }
+// }
+
+// num()
+
+// if(1 !== 1){
+// alert("Hi")
+// }
+
+
+const cities = ["Karachi" , "Lahore" , "Hyderabad"]
+// cities.push("Multan")
+// cities.shift()
+// cities.unshift("Islamabad")
+console.log(cities);
+
+console.log(cities);
+
+// cities.pop()
+// console.log(cities);
+
+// console.log(cities.at(2));
+
+// const findCities = cities.find(city=>{
+// console.log(city);
+// if(city == "Hyderabad"){
+// return 65
+// }
+// })
+
+// console.log(findCities);
