1af801e6269c852b70b7256a6783699efe30f0d6
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-05-29 18:07:55 +0500

    Whole js book practice

--- Whole js Book Practie/app.js
+++ Whole js Book Practie/app.js
@@ -15,7 +15,7 @@
 
 // const b = 6
 // console.log(b);
- 
+
 // var a= 87
 // var a = 84
 // console.log(a);
@@ -31,8 +31,8 @@
 
 // function num() {
 // if(1 == 1) {
-    // alert("Hello")  
-    // alert("Hello 2")
+// alert("Hello")  
+// alert("Hello 2")
 
 // }
 // }
@@ -44,13 +44,17 @@
 // }
 
 
-const cities = ["Karachi" , "Lahore" , "Hyderabad"]
+// const cities = ["Karachi" , "Lahore" , "Hyderabad" , "Quetta" , "Multan" , "Rawalpindi"]
 // cities.push("Multan")
 // cities.shift()
 // cities.unshift("Islamabad")
-console.log(cities);
+// const newArray =  cities.slice(0 , 2)
+// const newArray = cities.slice(-4 , -1)
+// cities.splice(1 , 2 , "Murree" , "Sawat")
+// console.log(cities);
+// console.log(newArray , `newArr`);
 
-console.log(cities);
+// console.log(cities);
 
 // cities.pop()
 // console.log(cities);
@@ -65,3 +69,111 @@ console.log(cities);
 // })
 
 // console.log(findCities);
+
+
+
+
+//     const tableNumber = +prompt("Enter your table number")
+// for(var i = 1 ; i <= 12 ; i++){
+// console.log(`${tableNumber} x ${i} = ${i*tableNumber}`);
+
+// }
+
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+
+//     }
+// }
+
+
+// const randomNum = Math.floor(Math.random() * (20 - 5 + 1) + 5) 
+// console.log(randomNum);
+
+// const str = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis distinctio voluptas aliquid placeat, obcaecati quia consequuntur? Veniam ea fugit sint perspiciatis inventore aut unde consequatur veritatis deserunt praesentium, quisquam, omnis voluptates corrupti repudiandae quidem assumenda fugiat voluptatum dolorem. Nemo, quaerat!`
+// const arr = []
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+
+// function  capitializeCase() {
+    // for(var i = 0 ; i < str.length ; i++){
+//  console.log(str.slice(i , i+2))
+//  if(str.slice(i , i+2) == "  "){
+// alert("Double space not allow")
+// return 
+// }
+// }
+// const newArr = str.split(" ")
+// console.log(newArr);
+
+// for( word of newArr){
+// console.log(word);
+// const separateWord = word.slice(0 , 1).toUpperCase() + word.slice(1).toLowerCase()
+// console.log(separateWord , `separateWord`);
+// arr.push(separateWord)
+// }
+
+// }
+
+// capitializeCase()
+
+// console.log(arr);
+
+// const capitializeWord = arr.join(" ")
+// console.log(capitializeWord);
+
+// function sum(num1 , num2) {
+// return num1 + num2
+// }
+
+
+// alert(sum(3, 2))
+
+
+
+// function foo(num1 = 100 , num2 = 200) {
+// console.log(num1 + num2);
+
+// }
+
+// foo(300 , 400)
+// var a;
+// function foo() {
+// a =700
+// }
+
+// foo()
+// console.log(a);
+
+
+// const day = prompt("aaj kia din hai")
+// if(day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday"){
+// alert(console.log("Work day"))
+// }else if(day === "Friday"){
+// alert("Short day")
+// }else if(day === "Saturday" || day === "Sunday"){
+// alert("WOW")
+// }else{
+//     alert("Please write correct day")
+// }
+
+
+// switch(day){
+// case "Monday" : console.log("Work day")
+// break
+// case "Tuesday" : console.log("Work day")
+// break
+// case "Wednesday" :console.log("Work day")
+// break 
+// case "Thursday" :console.log("Work day")
+// break 
+// case "Friday" :console.log("Short day")
+// break 
+// case "Saturday" : console.log("Wow")
+// break
+// case "Sunday" : console.log("Wow")
+// break
+// default : console.log("Invalid Day");
+
+// }
